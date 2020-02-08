f660a9334f6a6107f00867010037a2a53fee9028
auxfuse <aux.fuse@yahoo.com>
2020-02-08 20:44:55 +0000

    pages app created with basic view created with urls.py

---
+++ pages/urls.py
@@ -0,0 +1,16 @@
+'''
+Imports the path() function. This import is necessary for the URL dispatcher
+to work and is common to all urls.py files.
+'''
+from django.urls import path
+
+'''
+Imports the local views.py file. The dot operator (“.”) in this case is 
+shorthand for the current package, so this is saying “import all views from 
+the current package (pages)”.
+'''
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index')
+]
