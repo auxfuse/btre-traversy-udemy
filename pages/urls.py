c7ffcc036ff77ad751c22922362762bd520ef1a9
auxfuse <aux.fuse@yahoo.com>
2020-02-09 13:39:53 +0000

    Added templates for pages app with index and about page

--- pages/urls.py
+++ pages/urls.py
@@ -12,5 +12,6 @@ the current package (pages)”.
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('about', views.about, name='about'),
 ]
