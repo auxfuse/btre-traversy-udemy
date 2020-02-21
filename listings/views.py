34326fd88150800544f9ace6f96980d0049c862c
auxfuse <aux.fuse@yahoo.com>
2020-02-21 00:09:37 +0000

    listings.html template pagination added, along with ordering & filtered of rendered records

--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 
 from .models import Listing
@@ -6,15 +7,20 @@ from .models import Listing
 
 # Create your views here.
 def index(request):
-    listings = Listing.objects.all
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
     content = {
-        'listings': listings
+        'listings': paged_listings
     }
 
     return render(request, 'listings/listings.html', content)
 
 
-def listing(request):
+def listing(request, listing_id):
     return render(request, 'listings/listing.html')
 
 
