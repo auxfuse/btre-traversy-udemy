db57ac45b989ffe0d61502aa2324d383793b0414
auxfuse <aux.fuse@yahoo.com>
2020-02-22 22:43:23 +0000

    Search functionality built and implemented on for home page and search.html

--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .choices import price_choices, bedroom_choices, state_choices
 
 
 from .models import Listing
@@ -31,4 +32,24 @@ def listing(request, listing_id):
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    # Keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains=keywords)
+
+    # City
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+           queryset_list = queryset_list.filter(city__iexact=city)
+
+    context = {
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices,
+        'state_choices': state_choices,
+        'listings': queryset_list
+    }
+    return render(request, 'listings/search.html', context)
